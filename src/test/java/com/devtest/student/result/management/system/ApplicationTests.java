beed7fa25f8321cd358f3cccf664ef8c9e14b6cd
Akhila <akhila.perabattini@oracle.com>
2023-04-14 15:41:06 +0530

    Initialised Spring Boot project for Student Result Management System

---
+++ src/test/java/com/devtest/student/result/management/system/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtest.student.result.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
